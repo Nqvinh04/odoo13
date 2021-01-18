51201719e31dd925d576ad77adc86d6a4deda9f4
nqvinh04 <nqvinh1995@gmail.com>
2021-01-18 13:35:53 +0700

    Calendar View

--- custom_odoo/om_academy/models/session.py
+++ custom_odoo/om_academy/models/session.py
@@ -1,4 +1,6 @@
-from odoo import models, fields, api, _
+from datetime import timedelta
+
+from odoo import models, fields, api, exceptions, _
 
 
 class Session(models.Model):
@@ -9,13 +11,17 @@ class Session(models.Model):
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    active = fields.Boolean(default=True)
+    active = fields.Boolean()
     color = fields.Integer()
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('academy.course', ondelete='cascade',
                                 string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string='Attendee')
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string="End Date", store=True,
+                           compute='_get_end_date', inverse='_set_end_date')
+
+    attendee_count = fields.Integer(string="Attendee count", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -42,3 +48,29 @@ class Session(models.Model):
                     'message': _("Increase seats or remove excess attendees"),
                 }
             }
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+            r.duration = (r.end_date - r.start_date).days + 1
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendee_count = len(r.attendee_ids)
